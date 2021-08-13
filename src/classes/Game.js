047da5a0132f3b6479dd58edbc4c69f00c89d38d
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-08-13 20:23:52 +0200

    complete main game functionality

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -21,9 +21,9 @@ export default class Game {
     this.attemptsContainer.textContent = this.numberOfAttempts;
   }
 
-  #enableButtons() {
+  #toggleButtonsDisable(buttonDisabled) {
     const buttons = document.querySelectorAll('.board__letter');
-    buttons.forEach((button) => (button.disabled = false));
+    buttons.forEach((button) => (button.disabled = buttonDisabled));
   }
 
   #createLetter(className) {
@@ -68,7 +68,7 @@ export default class Game {
     e.preventDefault();
     this.#setPhrase();
     this.#renderPhrase();
-    this.#enableButtons();
+    this.#toggleButtonsDisable(false);
     this.#setNumberOfAttempts(5);
   }
 
@@ -88,8 +88,57 @@ export default class Game {
     }
   }
 
-  #handleButtonsClick(e) {
-    e.target.disabled = true;
+  #revealTheLetters(target) {
+    const arrayOfPhrase = [...this.phrase];
+    const phraseLetters = document.querySelectorAll('.phrase__letter');
+    let userGuessedLetterRight = false;
+    arrayOfPhrase.forEach((letter, index) => {
+      if (target.textContent === letter.toUpperCase()) {
+        phraseLetters[index].textContent = target.textContent;
+        if (!userGuessedLetterRight) userGuessedLetterRight = true;
+      }
+    });
+    return userGuessedLetterRight;
+  }
+
+  #revealThePhrase() {
+    const arrayOfPhrase = [...this.phrase];
+    const phraseLetters = document.querySelectorAll('.phrase__letter');
+    arrayOfPhrase.forEach((letter, index) => {
+      phraseLetters[index].textContent = letter.toUpperCase();
+    });
+  }
+
+  #checkIfGameOver() {
+    if (this.numberOfAttempts === 0) {
+      this.#toggleButtonsDisable(true);
+      this.#revealThePhrase();
+      return alert('Game is over. You loose!');
+    }
+    const phraseLetters = Array.from(
+      document.querySelectorAll('.phrase__letter')
+    );
+    let isPhraseGuessed = true;
+    for (let i = 0; i < phraseLetters.length; i++) {
+      if (phraseLetters[i].textContent === '') {
+        isPhraseGuessed = false;
+        break;
+      }
+    }
+
+    if (isPhraseGuessed) {
+      this.#toggleButtonsDisable(true);
+      return alert('You win. Congratulations!');
+    }
+  }
+
+  #handleButtonsClick({ target }) {
+    target.disabled = true;
+    const userGuessedLetterRight = this.#revealTheLetters(target);
+    if (!userGuessedLetterRight) {
+      this.#setNumberOfAttempts(--this.numberOfAttempts);
+    }
+    this.#checkIfGameOver();
   }
 
   #addEventListenerToButtons() {
